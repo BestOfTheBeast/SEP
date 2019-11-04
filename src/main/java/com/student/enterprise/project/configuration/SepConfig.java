b486b4bd0fb490ff9026d8f1ea140cfb7c1aabf1
StudentLgotnik <kazzarin99@gmail.com>
2019-11-04 22:28:24 +0200

    init

---
+++ src/main/java/com/student/enterprise/project/configuration/SepConfig.java
@@ -0,0 +1,10 @@
+package com.student.enterprise.project.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SepConfig {
+}
