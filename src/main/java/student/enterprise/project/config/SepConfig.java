a5dc384c0c308fdbc94f07664cbbfe4205ed3264
Stas <36239821+iSan4eZ@users.noreply.github.com>
2019-11-11 18:26:12 +0200

    Swagger Addition
    * Swagger Added
    
    * Swagger UI Added

---
+++ src/main/java/student/enterprise/project/config/SepConfig.java
@@ -0,0 +1,13 @@
+package student.enterprise.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Configuration for core services.
+ */
+@Configuration
+@EnableSwagger2
+public class SepConfig {
+
+}
--- src/main/java/student/enterprise/project/configuration/SepConfig.java
+++
@@ -1,8 +0,0 @@
-package student.enterprise.project.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SepConfig {
-
-}
