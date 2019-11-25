c0a0b7881913655b4740ef6775e3a544b4a038a0
Stas Panasiuk <temnyip@gmail.com>
2019-11-25 19:37:44 +0200

    Added abstract converters and some implementations as an example

---
+++ src/main/java/student/enterprise/project/converter/Converter.java
@@ -0,0 +1,14 @@
+package student.enterprise.project.converter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+@Component
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Converter {
+
+}
