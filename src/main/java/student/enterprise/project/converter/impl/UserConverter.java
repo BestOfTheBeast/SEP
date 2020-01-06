2610a2bb5c622b71fca101584f65ba4dc91a97cc
Maxim <31449825+StudentLgotnik@users.noreply.github.com>
2020-01-06 14:45:04 +0200

    UserDTO and UserConverter were changed (#15)

--- src/main/java/student/enterprise/project/converter/impl/UserConverter.java
+++ src/main/java/student/enterprise/project/converter/impl/UserConverter.java
@@ -6,26 +6,11 @@ import student.enterprise.project.converter.Converter;
 import student.enterprise.project.dto.UserDTO;
 import student.enterprise.project.entity.UserEntity;
 
-import java.util.stream.Collectors;
-
 @Converter
 public class UserConverter extends AbstractConverter<UserEntity, UserDTO> {
 
-    private GroupConverter groupConverter;
-
-    public UserConverter(ModelMapper mapper, GroupConverter groupConverter) {
+    public UserConverter(ModelMapper mapper) {
         super(UserEntity.class, UserDTO.class, mapper);
-        this.groupConverter = groupConverter;
     }
 
-    @Override
-    public UserDTO toDto(UserEntity entity) {
-        return UserDTO.builder()
-            .id(entity.getId())
-            .login(entity.getLogin())
-            .groupList(entity.getGroupRoleList().stream()
-                .map(groupRole -> groupConverter.toDto(groupRole.getGroup()))
-                .collect(Collectors.toList()))
-            .build();
-    }
 }
