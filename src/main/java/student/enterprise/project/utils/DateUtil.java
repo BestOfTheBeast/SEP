0c860d3d05babb15d871ef5fd04c1008d569d0e9
Stas Panasiuk <temnyip@gmail.com>
2020-01-14 14:42:58 +0200

    #001 - Added Utils tests

--- src/main/java/student/enterprise/project/utils/DateUtil.java
+++ src/main/java/student/enterprise/project/utils/DateUtil.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public final class DateUtil {
 
   //TODO Implement first/second week logic
-  //Returns 1 if it's a Monday of the first week and 14 if Sunday of the second
+  //Returns 0 if it's a Monday of the first week and 13 if Sunday of the second
   public static Integer getDayOfTwoWeeks(LocalDate localDate) {
     return 1;
   }
--- src/main/java/student/enterprise/project/utils/NumberUtil.java
+++ src/main/java/student/enterprise/project/utils/NumberUtil.java
@@ -2,7 +2,7 @@ package student.enterprise.project.utils;
 
 public final class NumberUtil {
 
-  //1 to get first bit and 16 to get the last one
+  //0 to get first bit and 15 to get the last one
   public static Boolean getBitAtPosition(short source, int position) {
     return ((source >> position) & 1) == 1;
   }
