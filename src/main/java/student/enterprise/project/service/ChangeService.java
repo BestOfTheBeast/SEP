a214e586ca276d65ea599eaa85b71dd544e0d427
Stas Panasiuk <temnyip@gmail.com>
2020-01-15 13:04:11 +0200

    Revert "#001 - ChangeService implementation logic change"
    This reverts commit 06c8c63f

--- src/main/java/student/enterprise/project/service/ChangeService.java
+++ src/main/java/student/enterprise/project/service/ChangeService.java
@@ -1,29 +1,15 @@
 package student.enterprise.project.service;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import student.enterprise.project.converter.impl.ChangeEntityConverter;
 import student.enterprise.project.dto.ChangeDTO;
 import student.enterprise.project.entity.ChangeEntity;
 
 @Service
-@RequiredArgsConstructor
-public abstract class ChangeService {
+public interface ChangeService {
 
-    private final ChangeEntityConverter changeConverter;
+    List<ChangeDTO> toDto(ChangeEntity entity);
 
-    protected List<ChangeDTO> toDto(ChangeEntity entity) {
-        return Arrays.stream(changeConverter.toDto(entity))
-            .collect(Collectors.toList());
-    }
-
-    protected List<ChangeDTO> toDto(List<ChangeEntity> entityList) {
-        return entityList.stream()
-            .flatMap(entity -> this.toDto(entity).stream())
-            .collect(Collectors.toList());
-    }
+    List<ChangeDTO> toDto(List<ChangeEntity> entity);
 
 }
