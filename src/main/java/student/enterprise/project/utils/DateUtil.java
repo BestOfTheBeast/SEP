94b844c592c2b52098334ffe296a29cf9e2150bc
Stas Panasiuk <temnyip@gmail.com>
2020-01-15 12:49:14 +0200

    #001 - DateUtil first/second weeks logic

--- src/main/java/student/enterprise/project/utils/DateUtil.java
+++ src/main/java/student/enterprise/project/utils/DateUtil.java
@@ -1,13 +1,20 @@
 package student.enterprise.project.utils;
 
 import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 
 public final class DateUtil {
 
-  //TODO Implement first/second week logic
   //Returns 0 if it's a Monday of the first week and 13 if Sunday of the second
   public static Integer getDayOfTwoWeeks(LocalDate localDate) {
-    return 1;
+    return (localDate.getDayOfWeek().getValue() - 1) + 7 * getWeekNumber(localDate);
+  }
+
+  //0 if first week, 1 if second
+  public static Integer getWeekNumber(LocalDate localDate) {
+    final WeekFields wf = WeekFields.of(Locale.getDefault());
+    return localDate.get(wf.weekOfWeekBasedYear()) % 2;
   }
 
 }
