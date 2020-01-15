06c8c63fa94d09a572883f620ae29393eb02211e
Stas Panasiuk <temnyip@gmail.com>
2020-01-15 13:00:39 +0200

    #001 - ChangeService implementation logic change

--- src/main/java/student/enterprise/project/service/ChangeService.java
+++ src/main/java/student/enterprise/project/service/ChangeService.java
@@ -1,15 +1,29 @@
 package student.enterprise.project.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import student.enterprise.project.converter.impl.ChangeEntityConverter;
 import student.enterprise.project.dto.ChangeDTO;
 import student.enterprise.project.entity.ChangeEntity;
 
 @Service
-public interface ChangeService {
+@RequiredArgsConstructor
+public abstract class ChangeService {
 
-    List<ChangeDTO> toDto(ChangeEntity entity);
+    private final ChangeEntityConverter changeConverter;
 
-    List<ChangeDTO> toDto(List<ChangeEntity> entity);
+    protected List<ChangeDTO> toDto(ChangeEntity entity) {
+        return Arrays.stream(changeConverter.toDto(entity))
+            .collect(Collectors.toList());
+    }
+
+    protected List<ChangeDTO> toDto(List<ChangeEntity> entityList) {
+        return entityList.stream()
+            .flatMap(entity -> this.toDto(entity).stream())
+            .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/student/enterprise/project/service/impl/ChangeServiceImpl.java
+++ src/main/java/student/enterprise/project/service/impl/ChangeServiceImpl.java
@@ -1,30 +1,13 @@
 package student.enterprise.project.service.impl;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 import student.enterprise.project.converter.impl.ChangeEntityConverter;
-import student.enterprise.project.dto.ChangeDTO;
-import student.enterprise.project.entity.ChangeEntity;
 import student.enterprise.project.service.ChangeService;
 
-@RequiredArgsConstructor
 //TODO rename service
-public class ChangeServiceImpl implements ChangeService {
+public class ChangeServiceImpl extends ChangeService {
 
-  private final ChangeEntityConverter changeConverter;
-
-  @Override
-  public List<ChangeDTO> toDto(ChangeEntity entity) {
-    return Arrays.stream(changeConverter.toDto(entity))
-        .collect(Collectors.toList());
+  public ChangeServiceImpl(ChangeEntityConverter changeConverter) {
+    super(changeConverter);
   }
 
-  @Override
-  public List<ChangeDTO> toDto(List<ChangeEntity> entityList) {
-    return entityList.stream()
-        .flatMap(entity -> this.toDto(entity).stream())
-        .collect(Collectors.toList());
-  }
 }
